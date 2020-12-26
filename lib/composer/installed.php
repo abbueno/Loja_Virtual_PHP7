c0918ab2a1ec530162a3db45d583e306d2e958f0
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2020-12-26 20:50:12 -0300

    lv-php7
    lv-php7

---
+++ lib/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8c22574a5e48b6d1eef5915dadb69adbd5a4a96e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8c22574a5e48b6d1eef5915dadb69adbd5a4a96e',
+    ),
+    'smarty/smarty' => 
+    array (
+      'pretty_version' => 'v3.1.36',
+      'version' => '3.1.36.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd148f7ade295014fff77f89ee3d5b20d9d55451',
+    ),
+  ),
+);
